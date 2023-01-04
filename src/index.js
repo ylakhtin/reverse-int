0db52a6e72453e28c82b80566600cbd682870b86
Yuriy Lakhtin <yuriy.lakhtin@gmail.com>
2023-01-04 19:55:10 +0100

    init: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let s = "";
+    const n1 = String(n);
+    for (let i = n1.length-1; i >= 0; i--){
+      if (n1[i] != "-") {
+        s = s + n1[i];
+      }
+      console.log(n1[i]);
+    }
+    console.log(s);
+    return s;
 }
